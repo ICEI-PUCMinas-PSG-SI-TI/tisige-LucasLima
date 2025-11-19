134fdec6820186f52f41f67d43c3efe12f50cb7e
Lucas Lima <works.lucassl@gmail.com>
2025-11-19 00:05:40 -0300

    editar vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
    // Configure base path for GitHub Pages deployment
-  base: mode === "production" ? "tisige-LucasLima" : "/",
+  base: mode === "production" ? "/tisige-LucasLima" : "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
